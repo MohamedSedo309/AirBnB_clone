3515b7e580cc3ea2f92e876c3a3a1c7eb76a01fb
MohamedSedo309 <mohamedsedo1907@gmail.com>
2023-07-16 01:51:42 +0000

    updating help function

--- console.py
+++ console.py
@@ -47,8 +47,22 @@ class HBNBCommand(cmd.Cmd):
         print("")
         return True
     
-    def do_help(self, arg: str) -> bool | None:
-        return super().do_help(arg)
+    def do_help(self, arg):
+        """List available commands with "help" or detailed help with "help cmd"."""
+        if arg:
+            try:
+                doc = getattr(self, 'do_' + arg).__doc__
+                if doc:
+                    self.stdout.write('{}\n'.format(doc))
+                    return
+            except AttributeError:
+                pass
+            self.stdout.write('{} is not a valid command\n'.format(arg))
+        else:
+            commands = [cmd[3:] for cmd in dir(self) if cmd.startswith('do_')]
+            self.stdout.write('Available commands:\n')
+            self.stdout.write('\n'.join(sorted(commands)))
+            self.stdout.write('\n')
     
 if __name__ == '__main__':
-    HBNBCommand().cmdloop()shouldn
+    HBNBCommand().cmdloop()
