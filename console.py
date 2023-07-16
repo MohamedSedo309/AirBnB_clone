52bac60315db925f302b7d8ab141004c21ba98ff
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-07-17 02:34:31 +0300

    Update console.py

--- console.py
+++ console.py
@@ -3,6 +3,15 @@
 import cmd
 import re
 from shlex import split
+from models.base_model import BaseModel
+from models.__init__ import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
 
 
     
