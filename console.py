662c82fff01023eb9717f3d0cbc5a0fe343c31df
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-07-17 01:42:48 +0300

    Update console.py

--- console.py
+++ console.py
@@ -20,11 +20,8 @@ class HBNBCommand(cmd.Cmd):
     }
 
     def parse(arg):
-    print("input argument:", arg)
     curly_braces = re.search(r"\{(.*?)\}", arg)
     brackets = re.search(r"\[(.*?)\]", arg)
-    print("curly braces:", curly_braces)
-    print("brackets:", brackets)
     if curly_braces is None:
         if brackets is None:
             return [i.strip(",") for i in split(arg)]
@@ -33,6 +30,11 @@ class HBNBCommand(cmd.Cmd):
             retl = [i.strip(",") for i in lexer]
             retl.append(brackets.group())
             return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
     else:
         lexer = split(arg[:curly_braces.span()[0]])
         retl = [i.strip(",") for i in lexer]
