a5e9223c454000bed70ff768276d83fde8521a6c
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-07-17 01:18:03 +0300

    Update console.py

--- console.py
+++ console.py
@@ -4,22 +4,7 @@ import cmd
 import re
 from shlex import split
 
-def parse(arg):
-    curly_brackets = re.search(r"\{(.*?)\}", arg)
-    square_brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_brackets is None:
-        if square_brackets is None:
-            return [i.strip(",") for i in split(arg)]
-        else:
-            text_till_brackets = split(arg[:square_brackets.span()[0]])
-            splitted_tokens = [i.strip(",") for i in text_till_brackets]
-            splitted_tokens.append(square_brackets.group())
-            return splitted_tokens
-    else:
-        text_till_brackets = split(arg[:curly_brackets.span()[0]])
-        splitted_tokens = [i.strip(",") for i in text_till_brackets]
-        splitted_tokens.append(curly_brackets.group())
-        return splitted_tokens
+
     
 class HBNBCommand(cmd.Cmd):
     """create command interpreter"""
