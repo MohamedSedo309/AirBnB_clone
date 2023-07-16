551d8cc897ad6e5bcb035e4400f7dc97e021a129
MohamedSedo309 <110174037+MohamedSedo309@users.noreply.github.com>
2023-07-17 02:07:19 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -11,24 +11,24 @@ class BaseModel:
         """
         init new base model
         """
-        timeform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, timeform)
-                else:
-                    self.__dict__[k] = v
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
         else:
-            models.storage.new(self)
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def save(self):
         """update the updateded at field"""
-        self.updated_at = datetime.today()
-        models.storage.save()
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """return the dictionary of an intstance"""
